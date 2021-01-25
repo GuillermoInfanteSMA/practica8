3b4b6bf102bd86e8fe1cac6198a8e1a1f6c0f7d0
GuillermoInfanteSMA <jose.infante@smags.com.mx>
2021-01-25 13:17:56 -0600

    Revert "#2, Primeras líneas de código"
    This reverts commit 5c4ebd23f535245e6b0dc68e051286ab5ad7e8e5.

--- src/practicagit8/PracticaGit8.java
+++ src/practicagit8/PracticaGit8.java
@@ -16,7 +16,6 @@ public class PracticaGit8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Practica 8 de git");
     }
     
 }
